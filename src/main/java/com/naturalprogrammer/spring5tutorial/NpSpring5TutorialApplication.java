b1e888a221b74317d6c1318416ac310bf530b367
Sanjay Patel <skpatel20@gmail.com>
2017-06-17 17:02:59 +0530

    Newly generated project

---
+++ src/main/java/com/naturalprogrammer/spring5tutorial/NpSpring5TutorialApplication.java
@@ -0,0 +1,12 @@
+package com.naturalprogrammer.spring5tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpSpring5TutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NpSpring5TutorialApplication.class, args);
+	}
+}
