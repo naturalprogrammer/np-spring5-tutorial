1dd68bc86c74eae48552b2e82862f311bfd97b33
Sanjay Patel <skpatel20@gmail.com>
2017-06-19 12:45:33 +0530

    Application properties

--- src/main/java/com/naturalprogrammer/spring5tutorial/controllers/HelloController.java
+++ src/main/java/com/naturalprogrammer/spring5tutorial/controllers/HelloController.java
@@ -1,15 +1,19 @@
 package com.naturalprogrammer.spring5tutorial.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 	
+	@Value("${app.name}")
+	private String appName;
+	
 	@RequestMapping("/hello")
 	public String hello() {
 		
-		return "Hello, world";
+		return "Hello, world " + appName;
 	}
 
 }
