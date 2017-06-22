dbda39b426e82c53214634eba01ac1143a78819c
Sanjay Patel <skpatel20@gmail.com>
2017-06-22 20:25:53 +0530

    View Controllers

---
+++ src/main/java/com/naturalprogrammer/spring5tutorial/config/MvcConfig.java
@@ -0,0 +1,15 @@
+package com.naturalprogrammer.spring5tutorial.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+	
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+    }
+}
+ 
