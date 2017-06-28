e834fa862f45efce2943324338177d909efb278a
Sanjay Patel <skpatel20@gmail.com>
2017-06-28 13:45:40 +0530

    Reading request data

--- src/main/java/com/naturalprogrammer/spring5tutorial/controllers/HelloController.java
+++ src/main/java/com/naturalprogrammer/spring5tutorial/controllers/HelloController.java
@@ -1,7 +1,10 @@
 package com.naturalprogrammer.spring5tutorial.controllers;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.naturalprogrammer.spring5tutorial.utils.MyUtils;
@@ -19,6 +22,18 @@ public class HelloController {
 		
 		return "hello";
 	}
+	
+	@RequestMapping("/hello/{id}")
+	public String helloId(Model model,
+			@PathVariable int id,
+			Optional<String> name) {
+		
+		model.addAttribute("id", id);
+		model.addAttribute("name", name.orElse("No Name"));
+		
+		return "hello-id";
+	}
+
 	
 //	@RequestMapping("/hello")
 //	public ModelAndView hello() {
---
+++ src/main/webapp/WEB-INF/jsp/hello-id.jsp
@@ -0,0 +1,7 @@
+<%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8"%>
+<%@include file="includes/header.jsp"%>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
+
+<spring:message code="hello" />, ${name}, your id is ${id}!
+
+<%@include file="includes/footer.jsp"%>     
