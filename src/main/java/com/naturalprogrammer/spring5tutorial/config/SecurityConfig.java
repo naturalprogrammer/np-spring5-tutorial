fa66ec811b9a0fa0223316df0dc18db86549287c
Sanjay Patel <skpatel20@gmail.com>
2017-07-06 18:35:36 +0530

    Spring security 1

---
+++ src/main/java/com/naturalprogrammer/spring5tutorial/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.naturalprogrammer.spring5tutorial.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
